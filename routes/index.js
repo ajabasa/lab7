d02093fa0869b6a4f8f18fc738e097af33e88d2f
Andrew Jabasa <amjabasa@gmail.com>
2016-02-18 19:14:52 +0000

    analytics lab

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var projects = require('../projects.json');
-
 /*
  * GET home page.
  */
 
 exports.view = function(req, res){
   	res.render('index', projects);
+  	projects["grid"] = true;
+};
+
+exports.viewGrid = function(req, res){
+	
+	res.render('index.handlebars', projects);
+	projects["grid"] = false;
 };
