9a901681114e0b76137faf675ea1b9e1e8061f01
Andrew Jabasa <amjabasa@gmail.com>
2016-02-18 19:45:59 +0000

    Added experiment header

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,21 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
-  	res.render('index', projects);
-  	projects["grid"] = true;
+
+	var random_num = Math.random();
+	console.log(random_num);
+
+	if (random_num > 0) {
+	  projects['grid'] = false;
+	  res.render('index', projects);
+	} else {
+	  res.redirect('/grid');
+	}
+  	
 };
 
 exports.viewGrid = function(req, res){
-	
+	projects["grid"] = true;
 	res.render('index.handlebars', projects);
-	projects["grid"] = false;
+	
 };
